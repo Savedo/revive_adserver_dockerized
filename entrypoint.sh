aa6506142166c6838c7e9951499804ab21b2842a
parabolic <nvelkovski@gmail.com>
2017-08-17 14:10:23 +0200

    chown only if the folder is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,17 @@
 APACHE_CONF_DIR="/etc/apache2/"
 REVIVE_DIR="/var/www/html"
 
+# Copy and chown the files only if revive is not installed.
+# This is in case you are using nfs or efs.
+if [ ! -f "${REVIVE_DIR}/var/INSTALLED" ]; then
+  echo "Revive is not installed copying folder contents"
+  rsync -a /tmp/revive/ ${REVIVE_DIR}
+  chown -R www-data:www-data ${REVIVE_DIR}
+fi
+
+echo "Cleaning up"
+rm -rf /tmp/revive/
+
 # Start apache in the foreground
 source ${APACHE_CONF_DIR}/envvars
 # Tail is for the logs to go to stdout.
