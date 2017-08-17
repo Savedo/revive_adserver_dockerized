6324644921b6ad6b78985b8ab6200c0eef5c3f53
parabolic <nvelkovski@gmail.com>
2017-08-17 13:48:55 +0200

    do not chown or copy contents

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,6 @@
 APACHE_CONF_DIR="/etc/apache2/"
 REVIVE_DIR="/var/www/html"
 
-# Copy the files only if revive is not installed.
-if [ ! -f "${REVIVE_DIR}/var/INSTALLED" ]; then
-  echo "Revive is not installed copying folder contents"
-  rsync -a /tmp/revive/ ${REVIVE_DIR}
-fi
-
-echo "Cleaning up"
-rm -rf /tmp/revive/
-
-chown -R www-data:www-data ${REVIVE_DIR}
-
 # Start apache in the foreground
 source ${APACHE_CONF_DIR}/envvars
 # Tail is for the logs to go to stdout.
